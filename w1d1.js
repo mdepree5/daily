2fc46286eb9c6555f0ef36836e302e9e05f326a0
Mitch DePree <mdepree315@gmail.com>
2022-06-06 15:47:13 -0400

    add leetcode algorithm problems

--- w1d1.js
+++ w1d1.js
@@ -47,3 +47,71 @@ const maxSubArray = nums => {
 // const nums1 = [-2,1,-3,4,-1,2,1,-5,4]
 // console.log(maxSubArray(nums1)) // => 6
 
+const linkedListAddTwoNumbers = (l1, l2) => {
+  let sum = 0;
+  let current = new ListNode(0);
+  let result = current;
+  
+  while (l1 || l2) {
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+    
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+    sum = sum > 9 ? 1 : 0;
+  }
+  
+  if (sum) current.next = new ListNode(sum);
+  return result.next;
+};
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807
+
+
+const search = (nums, target) => {
+  let first = 0;
+  let last = nums.length - 1;
+  while (first <= last) {
+    let mid = Math.floor(((first + last) / 2));
+    if (nums[mid] < target) first = mid + 1;
+    else if (nums[mid] > target) last = mid - 1;
+    else return mid;
+  }
+  return -1;
+};
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+
+const badVersion = n => {
+  let left = 1;
+  let right = n;
+
+  while(left < right){
+    let mid = left + Math.floor((right - left) / 2);
+    if (isBadVersion(mid)) right = mid;
+    else left = mid + 1;
+  }
+  return left;
+}
+
+const searchInsertPosition = (nums, target) => {
+  let left = 0;
+  let right = nums.length;
+
+  while (left < right){
+    let mid = Math.floor((left + right) / 2);
+    
+    if (nums[mid] < target) left = mid + 1;
+    else right = mid;
+  }
+  return left;
+}
