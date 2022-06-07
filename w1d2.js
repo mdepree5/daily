ce39f883f0304ccb8f72afb771d7c73bd1771675
Mitch DePree <mdepree315@gmail.com>
2022-06-07 14:50:38 -0400

    structy island-graph

--- w1d2.js
+++ w1d2.js
@@ -275,4 +275,32 @@ const sumLinkedList = head => {
   }
   return sum;
 }
+// => 'W'/'L' grid. row-column traversal, adding count for each valid island
+const islandCount = grid => {
+  const visited = new Set();
+  let count = 0;
+  for (let r = 0; r < grid.length; r += 1) {
+    for (let c = 0; c < grid[0].length; c += 1) {
+      if (explore(grid, r, c, visited) === true) count += 1;
+    }
+  }
+  return count;
+}
+// => delta-driven, inbound r/c index, and visited-checking logic.
+const explore = (grid, r, c, visited) => {
+  const rowInBounds = 0 <= r && r < grid.length;
+  const colInBounds = 0 <= c && c < grid[0].length;
+  if (!rowInBounds || !colInBounds) return false;
 
+  if (grid[r][c] === 'W') return false;
+
+  const pos = r + ',' + c;
+  if (visited.has(pos)) return false;
+  visited.add(pos);
+
+  explore(grid, r - 1, c, visited);
+  explore(grid, r + 1, c, visited);
+  explore(grid, r, c - 1, visited);
+  explore(grid, r, c + 1, visited);
+  return true;
+}
