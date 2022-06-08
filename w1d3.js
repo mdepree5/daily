17c6f624f0d17bb584c133a78efd0e646601ce92
Mitch DePree <mdepree315@gmail.com>
2022-06-08 12:37:08 -0400

    best time to buy and sell

--- w1d3.js
+++ w1d3.js
@@ -21,4 +21,18 @@ const intersect = (arr1, arr2) => {
 const arr1 = [1, 2, 2, 1];
 const arr2 = [2, 2];
 console.log(intersect(arr1, arr2)); // => [2,2]
+// => initialize "buy price" and replace if smaller (as iterate through prices). replace profit when prices[i+1] > buyPrice is greater than current max profit
+const bestTimeToBuyAndSell = prices => {
+  let buyPrice = prices[0];
+  let profit = 0;
 
+  for (let i = 0; i < prices.length - 1; i++) {
+    let currProfit = prices[i + 1] - prices[i];
+
+    if(currProfit > 0){
+      if(prices[i] < buyPrice) buyPrice = prices[i];
+      if(prices[i + 1] - buyPrice > profit) profit = prices[i + 1] - buyPrice;
+    }
+  }
+  return profit;
+}
