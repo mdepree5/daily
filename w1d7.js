5472924f475fb7ed16d413ccb959b579d6c31fbd
Mitch <mdepree315@gmail.com>
2022-06-12 15:07:42 -0400

    add w1d7

--- w1d7.js
+++ w1d7.js
@@ -9,3 +9,12 @@ console.log(zeroes.fill(4)) // => [4, 4, 4, 4]
 
 const words = ['longing', 'rusted', 'furnace', 'daybreak']
 console.log(words.filter(word => word[1] === 'u')) // => ['rusted', 'furnace']
+
+
+
+
+
+
+
+
+
