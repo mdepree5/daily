57da9111e272fb359bb3d3e2799eb9e1a2ac4a50
Mitch <mdepree315@gmail.com>
2022-06-12 11:51:17 -0400

    array methods

---
+++ w1d7.js
@@ -0,0 +1,11 @@
+// todo ——————————————————————————————————————————————————————————————————————————————————
+// todo                               Sun June 12, 2022
+// todo ——————————————————————————————————————————————————————————————————————————————————
+console.log(Array.isArray([1, 2, 3])) // => true
+console.log(Array.isArray('string')) // => false
+
+const zeroes = [0, 0, 0, 0]
+console.log(zeroes.fill(4)) // => [4, 4, 4, 4]
+
+const words = ['longing', 'rusted', 'furnace', 'daybreak']
+console.log(words.filter(word => word[1] === 'u')) // => ['rusted', 'furnace']
